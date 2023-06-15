6789ce432bf7cf3c4c4cf80e762aa25807a73e8a
Evely Sousa <evelysousa@gmail.com>
2023-06-14 21:39:41 -0300

    aula alura 1

---
+++ aula_alura.py
@@ -0,0 +1,27 @@
+#print ("Brasil", "ganhou", "5", "titulos mundiais")
+class pais:str
+pais = "Italia"
+type(pais)
+class quantidade:int
+quantidade = 4
+type(quantidade)
+
+##print(pais,"ganhou",quantidade,"titulos mundiais!\n")
+
+class dia: int
+dia = 13
+type(dia)
+
+class mes:int
+mes = 6
+type(mes)
+
+class ano: int
+ano = 2023
+type(ano)
+
+##print(dia,mes,ano,sep="/")
+##declaração de variáveis, tipo associado a elas, 
+
+
+    
