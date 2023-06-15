e41d80be8514e9a797985f11497c4c3fd733b22f
Evely Sousa <evelysousa@gmail.com>
2023-06-14 22:43:52 -0300

    aula python

--- alura_aula1.py
+++ alura_aula1.py
@@ -4,22 +4,29 @@ num_secret = 42
 tentativas = 3
 chances = 1
 
-while (chances<=tentativas):
-    chute_num  = input("Digite o seu numero: ")
+for chances in range(1,tentativas + 1):
+    print("Tentativa {} de {}".format(chances, tentativas))
+    chute_num  = input("Digite um numero entre 1 e 100: ")
     print("Você digitou", chute_num)
-    print("Tentativa", chances,"de", tentativas)
+   
     chute = int(chute_num)
     acertou = num_secret == chute 
     chute_maior = chute > num_secret
     chute_menor = chute < num_secret
+
+    if(chute<1 or chute >100):
+       print("Você dee digitar um número de 1 a 100")
+       continue
+
     if (acertou):
         print("Você acertou!\n")
+        break
     else: 
         if(chute_maior):
             print("Chute errado! Seu chute foi maior do que o número secreto")
         elif(chute_menor):
             print("Chute errado! Seu chute foi menor do que o número secreto") 
-    chances=chances+1
+            
 print("**Fim do jogo**")
     
 
