415dda4e36d1a142b3d89c19e331825cddbab3b4
Evely Sousa <evelysousa@gmail.com>
2023-06-15 20:42:29 -0300

    implementação do import random

--- alura_aula1.py
+++ alura_aula1.py
@@ -1,32 +1,60 @@
+import random
+
 print("*Bem vindo ao nosso jogo*")
 
-num_secret = 42
-tentativas = 3
-chances = 1
+num_secret = random.randrange(1, 101)
+tentativas = 0
+pontuacao = 1000
 
-for chances in range(1,tentativas + 1):
+print("Escolha o nível de dificuldade")
+print("1- fácil 2- médio 3 - dificil")
+nivel= int(input("Escolha o nível de dificuldade: "))
+print("você escolheu o nível:", nivel)
+print("Você tem 1000 pontos para começar!")
+
+if(nivel == 1):
+    tentativas = 15
+elif(nivel==2):
+    tentativas = 10
+else:
+    tentativas = 5  
+
+for chances in range(1,tentativas + 2):
+    print("Você tem:", tentativas, "tentativas")
     print("Tentativa {} de {}".format(chances, tentativas))
+    
     chute_num  = input("Digite um numero entre 1 e 100: ")
     print("Você digitou", chute_num)
-   
     chute = int(chute_num)
-    acertou = num_secret == chute 
-    chute_maior = chute > num_secret
-    chute_menor = chute < num_secret
 
+    
     if(chute<1 or chute >100):
        print("Você dee digitar um número de 1 a 100")
        continue
 
+    acertou = num_secret == chute 
+    chute_maior = chute > num_secret
+    chute_menor = chute < num_secret
+
     if (acertou):
-        print("Você acertou!\n")
+        print("Você acertou e fez {}".format(pontuacao),)
         break
+         
     else: 
+        p_perdidos = abs(num_secret-chute)
+        pontuacao = pontuacao - p_perdidos 
+        
         if(chute_maior):
-            print("Chute errado! Seu chute foi maior do que o número secreto")
+            print("Seu chute foi maior do que o número secreto")
+            if(chances==tentativas):
+               print("O número secreto era: {}. Você fez {}". format(num_secret, pontuacao))     
         elif(chute_menor):
-            print("Chute errado! Seu chute foi menor do que o número secreto") 
-            
+            print("Seu chute foi menor do que o número secreto") 
+            if(chances==tentativas):
+               print("O número secreto era: {}. Você fez {}". format(num_secret, pontuacao)) 
+
+        
+        
 print("**Fim do jogo**")
     
 
