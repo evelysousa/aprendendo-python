310ab36894be36b56c4e78c1c0724871aa1811c2
Evely Sousa <evelysousa@gmail.com>
2023-06-14 21:36:50 -0300

    Inclusao aula  alura

---
+++ alura_aula.py
@@ -0,0 +1,25 @@
+print("*Bem vindo ao nosso jogo*")
+
+num_secret = 42
+tentativas = 3
+chances = 1
+
+while (chances<=tentativas):
+    chute_num  = input("Digite o seu numero: ")
+    print("Você digitou", chute_num)
+    print("Tentativa", chances,"de", tentativas)
+    chute = int(chute_num)
+    acertou = num_secret == chute 
+    chute_maior = chute > num_secret
+    chute_menor = chute < num_secret
+    if (acertou):
+        print("Você acertou!\n")
+    else: 
+        if(chute_maior):
+            print("Chute errado! Seu chute foi maior do que o número secreto")
+        elif(chute_menor):
+            print("Chute errado! Seu chute foi menor do que o número secreto") 
+    chances=chances+1
+print("**Fim do jogo**")
+    
+
